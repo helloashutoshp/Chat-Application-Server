96e222b296e6fe37394a6e0317d6f563a800ac5a
helloashutoshp <124667480+helloashutoshp@users.noreply.github.com>
2024-08-23 21:04:30 +0530

    done

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,12 +1,15 @@
-import { TryCatch } from "../middlewares/error";
-import { Errorhandler } from "../utils/utility";
+import { TryCatch } from "../middlewares/error.js";
+import { Errorhandler } from "../utils/utility.js";
 import { Chat } from "../models/chat.js";
+import { emitEvent } from "../utils/feature.js";
+import { ALERT, REFETCH_CHATS } from "../constants/event.js";
 
 const newGroupChat = TryCatch(async (req, res, next) => {
   const { name, members } = req.body;
   if (members.length < 2) {
     return next(new Errorhandler("GroupChat must have 3 members", 400));
   }
+
   const allMembers = [...members, req.user];
   await Chat.create({
     name,
@@ -14,6 +17,12 @@ const newGroupChat = TryCatch(async (req, res, next) => {
     members: allMembers,
     creator: req.user,
   });
+  emitEvent(req,ALERT,allMembers,`Welcome to ${name} group`);
+  emitEvent(req,REFETCH_CHATS,members);
+  return res.status(201).json({
+    success:true,
+    message:"created"
+  })
 });
 
 export { newGroupChat };
